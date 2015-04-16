609b00cc53c6a74aec61260096a2fdff5ea348a8
Johan Bjurling <johan.bjurling@gmail.com>
2015-04-16 10:30:30 +0200

    Removed unused dependencies.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,7 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-
-    requirejs: {
-      compile: {
-        options: {
-          baseUrl: "src",
-          mainConfigFile: "src/config.js",
-          name: "lib",
-          out: "dist/frame-bridge-<% pkg.version %>.min.js"
-        }
-      }
-    }
+    pkg: grunt.file.readJSON('package.json')
   });
 
 };
